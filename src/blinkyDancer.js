e38a8266bb13b7a32e9649001244c2c3c474869f
shaymmaharaj <shyammaharaj@gmail.com>
2019-04-15 20:01:15 -0700

    Implement new keyword for add blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -21,7 +21,6 @@
 
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
@@ -29,6 +28,6 @@ makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
-  makeDancer.prototype.step.call(this)
+  makeDancer.prototype.step.call(this);
   this.$node.toggle();
-}
+};
