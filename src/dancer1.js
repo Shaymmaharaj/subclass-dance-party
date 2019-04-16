fc0cd0bb1b8bbbe352c9d6ccadd2d510ef632257
shaymmaharaj <shyammaharaj@gmail.com>
2019-04-15 20:35:54 -0700

    Added new dancers

---
+++ src/dancer1.js
@@ -0,0 +1,20 @@
+var makeDancer = function(top, left, timeBetweenSteps) {
+    this.$node = $('<span class="dancer"></span>');
+    this.top = top;
+    this.left = left;
+    this.timeBetweenSteps = timeBetweenSteps;
+    this.step();
+    this.setPosition(top, left);
+  };
+  
+  makeDancer.prototype.step = function() {
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  };
+  
+  makeDancer.prototype.setPosition = function(top, left) {
+    var styleSettings = {
+      top: top,
+      left: left,
+    }
+    this.$node.css(styleSettings);
+  };
